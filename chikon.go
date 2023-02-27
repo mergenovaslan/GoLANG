7a1abf6b2e21f89a38752837f5decd6f8077f11c
mergenovaslan <91003849+mergenovaslan@users.noreply.github.com>
2023-02-28 01:07:32 +0600

    Update chikon.go

--- chikon.go
+++ chikon.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"net/http"
 )
@@ -18,20 +20,46 @@ func (m Main) getAll() string {
 }
 func home(w http.ResponseWriter, r *http.Request) {
 	chikon := Main{"Chikon", 20, "Female", 5000}
-	//fmt.Fprintf(w, chikon.getAll())
-	templ, _ := template.ParseFiles("templates/home_page.html")
+	templ, _ := template.ParseFiles("templates/home_page.html", "templates/second.html")
 	templ.Execute(w, chikon)
 }
 
 func second(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Second Page")
+	sec := Main{"Diana", 20, "Female", 570}
+	templ, _ := template.ParseFiles("templates/second.html")
+	templ.Execute(w, sec)
+}
+func save(w http.ResponseWriter, r *http.Request) {
+	ID := r.FormValue("ID")
+	NAME := r.FormValue("NAME")
+	SURNAME := r.FormValue("SURNAME")
+	AGE := r.FormValue("AGE")
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Access")
+
+	insert, err := db.Query(fmt.Sprintf("Insert Into 'users' ('ID','NAME','SURNAME','AGE') Values('%s','%s','%s','%s')", ID, NAME, SURNAME, AGE))
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	http.RedirectHandler("/", 304)
 }
 
 func handleRequest() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/sec/", second)
+	http.HandleFunc("/save", save)
 	http.ListenAndServe(":5555", nil)
 }
 func main() {
+
 	handleRequest()
+
 }
